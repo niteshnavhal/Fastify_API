70810bddf6f66792dbf0065ea96d052321d0b341
Nitesh Navhal <78400237+niteshnavhal@users.noreply.github.com>
2023-12-02 12:40:49 +0530

    Update config.js

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -3,15 +3,17 @@ module.exports = () => {
   if (process.env.DB == "production") {
   } else {
     posrgreSqlDatabase = {
-      username: process.env.POSTGRES_USERNAME,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      dialect: process.env.POSTGRES_DIALECT,
-      logging: false,
-      ssl: {
-        rejectUnauthorized: false, // This is for testing purposes, consider setting it properly
+      username: "admin",
+      password: "1Qb4NkNWFSZnLySkPCATB1TeAMNYsa",
+      database: "postgres",
+      host: "us-west1.ad283f5d-7f98-4886-ab74-50c0e4757bda.gcp.ybdb.io",
+      port: 5433,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // This is for testing purposes, consider setting it properly
+        },
       },
     };
   }
